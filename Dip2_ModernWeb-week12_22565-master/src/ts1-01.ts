9aa600f3a5e679499bbfa3941a85bd5c8dcb544d
tatobyw <tetocandy@gmail.com>
2023-02-07 15:49:03 +0700

    up

---
+++ Dip2_ModernWeb-week12_22565-master/src/ts1-01.ts
@@ -0,0 +1,7 @@
+const msg:string = "Hello"
+const fname:string = "Mark Zuckerberg"
+const province:string = "Trat"
+
+// console.log(msg + '  ' + fname)
+console.log(`${msg} ${fname}`)
+console.log(province)
---
+++ Dip2_ModernWeb-week12_22565-master/src/ts1-02.ts
@@ -0,0 +1,5 @@
+let counter:number = 100 //inferred type
+let stdName: string = 'Mark' //explicit type
+let isActivate: boolean = true //explicit type
+const numberList: number[] = [10, 20, 30] //explicit type
+
